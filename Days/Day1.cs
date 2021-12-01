2d5baa3287e4845a0acfcff6907816a40d02c1c4
phrodide <github@phrodide.com>
2021-11-30 21:42:00 -0700

    Add project files.

---
+++ Days/Day1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2021.Days
+{
+    internal class Day1
+    {
+        public Day1()
+        {
+               
+        }
+
+        public string SolvePart1()
+        {
+            return "";
+        }
+
+        public string SolvePart2()
+        {
+            return "";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+var day = new AoC2021.Days.Day1();
+Console.WriteLine("Part 1:");
+Console.WriteLine(day.SolvePart1());
+Console.WriteLine("Part 2:");
+Console.WriteLine(day.SolvePart2());
